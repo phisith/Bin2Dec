4c785ab3ad3d58a7beaf69eb8e5470b1d94efa56
phisith <aham226@hotmail.com>
2023-02-12 12:26:45 +0700

    add input validation and error messasge and button click input (number, clear, and sum)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import Button from "./Button";
-
+import { useState } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const [preInput, setPreInput] = useState("");
+  const [inputError, setInputError] = useState(false);
+  const [result, setResult] = useState("");
+
+  const regexCheck = new RegExp(/^[01]+$/g);
+  const onChangeInput = (value: string) => {
+    setInputError(!regexCheck.test(value));
+    setInput(value);
+  };
   return (
     <div className="flex flex-col h-screen justify-center items-center bg-gray-900 gap-6">
       <h1 className="text-white font-extrabold text-2xl">
@@ -8,14 +18,44 @@ function App() {
       </h1>
       <div className="flex flex-col border border-white/5 bg-white/5 rounded-xl shadow-xl p-4 gap-4">
         <div className="flex flex-col text-right border border-white/5 bg-white/5 rounded-xl p-4 gap-2">
-          <span className="text-gray-400 text-xl">001010 =</span>
-          <input className="text-right text-4xl bg-transparent text-white" />
+          <span className="text-gray-400 text-xl">
+            {preInput} {preInput && "="}
+          </span>
+          <input
+            className="text-right text-4xl bg-transparent text-white"
+            value={input}
+            onChange={(e) => onChangeInput(e.target.value)}
+          />
         </div>
+        {input ? (
+          <div
+            className={`text-center font-medium ${
+              inputError ? "visible text-red-600" : "hidden"
+            }`}
+          >
+            Please enter only 0 and 1
+          </div>
+        ) : null}
         <div className="flex gap-4">
-          <Button title={"C"} />
-          <Button title={"0"} />
-          <Button title={"1"} />
-          <Button title={"="} isSum={true} />
+          <Button
+            title={"C"}
+            onClickAction={() => {
+              setInput(""), setPreInput("");
+            }}
+          />
+          <Button
+            title={"0"}
+            onClickAction={() => setInput((prev) => prev + "0")}
+          />
+          <Button
+            title={"1"}
+            onClickAction={() => setInput((prev) => prev + "1")}
+          />
+          <Button
+            title={"="}
+            isSum={true}
+            onClickAction={() => setPreInput(input)}
+          />
         </div>
       </div>
     </div>
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,8 +1,9 @@
 interface PropName {
   title: string;
   isSum?: boolean;
+  onClickAction: Function;
 }
-export default function Button({ title, isSum }: PropName) {
+export default function Button({ title, isSum, onClickAction }: PropName) {
   return (
     <button
       className={`w-full border rounded-xl text-white ${
@@ -10,6 +11,7 @@ export default function Button({ title, isSum }: PropName) {
           ? "border-orange-500 bg-orange-500 hover:border-orange-400 hover:bg-orange-400 active:border-orange-500/70 active:bg-orange-500/70"
           : "border-white/10 bg-white/10 hover:border-white/20 hover:bg-white/20 active:border-gray-100/10 active:bg-gray-100/10"
       } py-8 px-10 font-bold text-4xl`}
+      onClick={(e) => onClickAction(e)}
     >
       {title}
     </button>
